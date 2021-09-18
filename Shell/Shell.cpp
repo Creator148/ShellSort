6348db137672fcb8ada53d2363c74abdf6aa9179
Creator148 <bak-home@mail.ru>
2021-09-18 22:06:35 +0300

    Должно работать(вроде)

--- Shell/Shell.cpp
+++ Shell/Shell.cpp
@@ -56,41 +56,39 @@ int  main() {
 	return 0;
 }
 
-void Myshell(){
-		int R[16] = { 503,87,512,61,908,170,897,275,653,426,154,509,612,677,765,703 };
-	int N, K,q = 5;
-	int h[5] = { 1,2,3,5,7 };
-		for (int s = h[q - 1]; q != 0; q--) {
-			s = h[q - 1];
-			for (int p = 0; p < 16; p++) {
-				cout << R[p] << ' ';
-				if (p == 15) {
-					cout << endl;
-				}
-			}
-			for (int i = 0; i < 16; i++) {
-				for (int j = i + s; j < 16; j += s) {
-					if (R[i] > R[j]) {
-						K = R[j];
-						R[j] = R[i];
-						R[i] = K;
-					}
-				}
-			}
-		}
+void Myshell(int* h, int* R) {
+	int N, K, q = 5;
+	for (int s = h[q - 1]; q != 0; q--) {
+		s = h[q - 1];
 		for (int p = 0; p < 16; p++) {
 			cout << R[p] << ' ';
 			if (p == 15) {
 				cout << endl;
 			}
 		}
-
-
+		for (int i = 0; i < 16; i++) {
+			for (int j = i + s; j < 16; j += s) {
+				if (R[i] > R[j]) {
+					K = R[j];
+					R[j] = R[i];
+					R[i] = K;
+				}
+			}
+		}
+	}
+	for (int p = 0; p < 16; p++) {
+		cout << R[p] << ' ';
+		if (p == 15) {
+			cout << endl;
+		}
+	}
 }
 
 
-void StandardShell(int size,int* R[]){
-	int N, K;
+
+
+void StandardShell(int size, int* R) {
+	int N, K = 0;
 	for (int s = size / 2; s > 0; s /= 2) {
 		for (int p = 0; p < 16; p++) {
 			cout << R[p] << ' ';
@@ -114,7 +112,6 @@ void StandardShell(int size,int* R[]){
 			cout << endl;
 		}
 	}
-
 }
 
 

5e441b32a1c12e94ebd5d248a948711edb2904d7
Creator148 <bak-home@mail.ru>
2021-10-11 23:34:47 +0300

    Всё переделано в нормальный вид и готово к работе.

--- Shell/Shell.cpp
+++ Shell/Shell.cpp
@@ -1,73 +1,24 @@
 ﻿
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <chrono>
 
 using namespace std;
 
-//int main()
-//{
-//	int k[17] = {0,503,87,512,61,908,170,897,275,653,426,154,509,612,677,765,703};
-//	int H[6] = {0,1,1,2,4,8};
-//	int s, h, R, i, K;
-//	int t = 6;
-//
-//D1:	t = t - 1;
-//	s = t;
-//	int j = 17;
-//	for (int p = 1; p < 17; p++) {
-//		cout << (k[p]) << ' ';
-//		if (p == 16) {
-//			cout << endl;
-//		}
-//	}
-//	if (s == 0){
-//	return 0;
-//	}
-//	goto D2;
-//
-//D2:	h = H[s];
-//	j = j - 1;
-//	goto D3;
-//
-//D3:	i = j - h;
-//	K = k[j];
-//	R = k[j];
-//	goto D4;
-//
-//D4:	if (K >= k[i]){
-//	goto D6;
-//}
-//	goto D5;
-//D5:	k[i+h] = k[i];
-//	i = i - h;
-//	if (i > 0) {
-//	goto D4;
-//	}
-//	goto D6;
-//D6:	k[i + h] = R;
-//	if (j <= 17 && j > h) {
-//		goto D2;
-//	}
-//	goto D1;
-//}
-
-
-int  main() {
-
-	return 0;
-}
-
-void Myshell(int* h, int* R) {
+void Myshell(int* h, int* R, int size) {
 	int N, K, q = 5;
 	for (int s = h[q - 1]; q != 0; q--) {
 		s = h[q - 1];
-		for (int p = 0; p < 16; p++) {
+		/*for (int p = 0; p < size; p++) {
 			cout << R[p] << ' ';
 			if (p == 15) {
 				cout << endl;
 			}
-		}
-		for (int i = 0; i < 16; i++) {
-			for (int j = i + s; j < 16; j += s) {
+		}*/
+		for (int i = 0; i < size; i++) {
+			for (int j = i + s; j < size; j += s) {
 				if (R[i] > R[j]) {
 					K = R[j];
 					R[j] = R[i];
@@ -76,28 +27,27 @@ void Myshell(int* h, int* R) {
 			}
 		}
 	}
-	for (int p = 0; p < 16; p++) {
+	/*for (int p = 0; p < size; p++) {
 		cout << R[p] << ' ';
 		if (p == 15) {
 			cout << endl;
 		}
-	}
+	}*/
 }
 
 
 
-
-void StandardShell(int size, int* R) {
-	int N, K = 0;
-	for (int s = size / 2; s > 0; s /= 2) {
-		for (int p = 0; p < 16; p++) {
-			cout << R[p] << ' ';
-			if (p == 15) {
-				cout << endl;
-			}
-		}
-		for (int i = 0; i < 16; i++) {
-			for (int j = i + s; j < 16; j += s) {
+void StandardShell(int h, int* R, int size) {
+	int K = 0;
+	for (int s = h / 2; s > 0; s /= 2) {
+		//for (int p = 0; p < size; p++) {
+		//	cout << R[p] << ' ';
+		//	if (p == 15) {
+		//		cout << endl;
+		//	}
+		//}
+		for (int i = 0; i < size; i++) {
+			for (int j = i + s; j < size; j += s) {
 				if (R[i] > R[j]) {
 					K = R[j];
 					R[j] = R[i];
@@ -106,13 +56,56 @@ void StandardShell(int size, int* R) {
 			}
 		}
 	}
-	for (int p = 0; p < 16; p++) {
+	/*for (int p = 0; p < size; p++) {
 		cout << R[p] << ' ';
 		if (p == 15) {
 			cout << endl;
 		}
-	}
+	}*/
 }
+
+
+int main() {
+	int* arr = new int[5000];
+	int key = 8;
+	int Hkey[5] = {1,6,9,11,15};
+	string fileName;
+	cin >> fileName;
+	ifstream currentFile(fileName, ios::in);
+	if (currentFile.is_open()) {
+		int N = 0;
+		string line;
+		while (getline(currentFile, line)) {
+			istringstream iss(line);
+			if (N >= 5000) {
+				cout << "File " + fileName + " has more than 5000 numbers" << endl;
+				return 0;
+			}
+			iss >> arr[N];
+			N++;
+		}
+		auto start = chrono::high_resolution_clock::now();
+		Myshell(Hkey,arr,N);
+		auto end = chrono::high_resolution_clock::now();
+		auto duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+		cout << duration << " microseconds" << endl;
+		ofstream newFile("d" + to_string(N) + "s.txt");
+		for (int i = 0; i < N; i++) {
+			newFile << arr[i] << endl;
+		}
+		newFile.close();
+		currentFile.close();
+		delete[] arr;
+	}
+	else {
+		cout << "Unable to open file" << endl;
+	}
+	return 0;
+}
+
+
+
+
 
 
 
